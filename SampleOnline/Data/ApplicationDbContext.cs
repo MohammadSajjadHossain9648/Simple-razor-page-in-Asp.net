03851ffb56c9c4b38bdb24553562f78028c79ab9
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2022-09-06 01:16:54 +0600

    setup program.cs to use dbcontext

---
+++ SampleOnline/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SampleOnline.Model;
+
+namespace SampleOnline.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+         : base(options)
+        {
+        }
+
+        public DbSet<Category> Category { get; set; } //create table in database by DbSet<model_class_name> Table_name
+    }
+}
